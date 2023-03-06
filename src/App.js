720d50b0e2a44e5aa62ef26355345e5748377c83
Shazreen Seatter <shazseatter12@gmail.com>
2023-03-06 20:05:45 +0000

    inital commit - extensions completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react'; 
+
+
 
 function App() {
+
+  const [todos, setTodos] = useState([
+    {name: "Buy shopping", priority: "high"},
+    {name: "Clean bathroom", priority: "low"},
+    {name: "Car's MOT", priority: "high"},
+  ])
+
+  const [newTodoName, setNewTodoName] = useState(''); 
+  const [value, setValue] = useState(false)
+
+  function handleInputChange(event) {
+    setNewTodoName(event.target.value)
+  }
+
+  function handleRadioChange (event) {
+    setValue(event.target.value); 
+  }
+
+  function saveNewTodo (event) {
+    event.preventDefault(); 
+
+    const newTodo = {
+      name: newTodoName,
+      priority: value,
+    }
+    
+    const newTodoList = [...todos, newTodo]
+
+    setTodos(newTodoList);
+    setNewTodoName('');
+  }
+
+
+  const todoNodes = todos.map((todo, index) => 
+   ( <li key={index}>
+
+    {todo.priority === "high" ? <span className='high'>{todo.name}</span> : <span className='low'>{todo.name}</span>}
+
+    </li>
+  ));
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>ToDo's</h1>
+      <form onSubmit={saveNewTodo}>
+        <label htmlFor="todo-name">Add New Todo:</label>
+        <input id="todo-name" type="text" value={newTodoName}onChange={handleInputChange}/>
+        High<input type="radio" value="high" checked={value === "high"} onChange={handleRadioChange}/> 
+        Low<input type="radio" value="low" checked={value === "low"} onChange={handleRadioChange}/> 
+        <input type="submit" value="Save Item"/>
+      </form>
+      <ul>
+        {todoNodes}
+      </ul>
     </div>
   );
 }
+
 
 export default App;
